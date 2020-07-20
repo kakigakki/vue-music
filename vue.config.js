90efcb1aa9f86ad393fc6064eb62b0ae7887d000
kakigakki <kakigakki@yahoo.co.jp>
2020-07-20 22:54:39 +0900

    init api

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // vue.config.js
 const path = require("path");
+const before = require("./qqmusic-data-conf");
 
 function resolve(dir) {
   return path.join(__dirname, dir);
@@ -11,5 +12,31 @@ module.exports = {
       .set("components", resolve("src/components"))
       .set("views", resolve("src/views"))
       .set("common", resolve("src/common"));
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: " https://u.y.qq.com/cgi-bin/musicu.fcg",
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        pathRewrite: {
+          "^/api": ""
+        }
+      },
+      "/v8": {
+        target: "https://c.y.qq.com/v8/fcg-bin/v8.fcg",
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        pathRewrite: {
+          "^/v8": ""
+        }
+      },
+      "/lyrics": {
+        target: "https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg",
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        pathRewrite: {
+          "^/lyrics": ""
+        }
+      }
+    },
+    before
   }
 };
