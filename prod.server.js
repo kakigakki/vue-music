d1862a54e502ff032d99acfe82fc09eddf3d15d3
kakigakki <kakigakki@yahoo.co.jp>
2020-12-05 02:38:16 +0900

    1

--- prod.server.js
+++ prod.server.js
@@ -4,6 +4,8 @@ var axios = require('axios')
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 var csrf = require('xsrf')
+var history = require('connect-history-api-fallback')
+
 var vueAxios = axios.create({
   headers: {
     origin: 'https://y.qq.com/',
@@ -33,100 +35,9 @@ var csrfProtection = csrf({
 })
 app.use(cookieParser())
 app.use(csrfProtection)
+app.use(history())
 
 var apiRoutes = express.Router()
-
-// apiRoutes.get('/getDiscList', csrfProtection, function(req, res) {
-//     var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
-//     axios.get(url, {
-//         headers: {
-//             referer: 'https://c.y.qq.com/',
-//             host: 'c.y.qq.com'
-//         },
-//         params: req.query
-//     }).then((response) => {
-//         res.json(response.data)
-//     }).catch((e) => {
-//         console.log(e)
-//     })
-// })
-
-// apiRoutes.get('/getCdInfo', csrfProtection, function(req, res) {
-//     var url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
-//     axios.get(url, {
-//         headers: {
-//             referer: 'https://c.y.qq.com/',
-//             host: 'c.y.qq.com'
-//         },
-//         params: req.query
-//     }).then((response) => {
-//         var ret = response.data
-//         if (typeof ret === 'string') {
-//             var reg = /^\w+\(({.+})\)$/
-//             var matches = ret.match(reg)
-//             if (matches) {
-//                 ret = JSON.parse(matches[1])
-//             }
-//         }
-//         res.json(ret)
-//     }).catch((e) => {
-//         console.log(e)
-//     })
-// })
-
-// apiRoutes.get('/lyric', csrfProtection, function(req, res) {
-//     var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
-
-//     axios.get(url, {
-//         headers: {
-//             referer: 'https://c.y.qq.com/',
-//             host: 'c.y.qq.com'
-//         },
-//         params: req.query
-//     }).then((response) => {
-//         var ret = response.data
-//         if (typeof ret === 'string') {
-//             var reg = /^\w+\(({.+})\)$/
-//             var matches = response.data.match(reg)
-//             if (matches) {
-//                 ret = JSON.parse(matches[1])
-//             }
-//         }
-//         res.json(ret)
-//     }).catch((e) => {
-//         console.log(e)
-//     })
-// })
-
-// apiRoutes.post('/getPurlUrl', bodyParser.json(), csrfProtection, function(req, res) {
-//     const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
-//     axios.post(url, req.body, {
-//         headers: {
-//             referer: 'https://y.qq.com/',
-//             origin: 'https://y.qq.com',
-//             'Content-type': 'application/x-www-form-urlencoded'
-//         }
-//     }).then((response) => {
-//         res.json(response.data)
-//     }).catch((e) => {
-//         console.log(e)
-//     })
-// })
-
-// apiRoutes.get('/search', csrfProtection, function(req, res) {
-//     const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
-//     axios.get(url, {
-//         headers: {
-//             referer: 'https://c.y.qq.com/',
-//             host: 'c.y.qq.com'
-//         },
-//         params: req.query
-//     }).then((response) => {
-//         res.json(response.data)
-//     }).catch((e) => {
-//         console.log(e)
-//     })
-// })
 
 // 获取 qq music 推荐页 recomPlayList信息
 apiRoutes.get('/getDiscList', csrfProtection, function (req, res) {
